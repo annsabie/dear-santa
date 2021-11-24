2cf27b8fef68ad2780f2669416de8a5ac24fc5d2
Cole Enyart <letchro@gmail.com>
2021-11-24 00:19:44 -0600

    Added wishlist form in profile page

--- client/src/components/wishlist/Wishlist.js
+++ client/src/components/wishlist/Wishlist.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import './wishlist.css';
+import React from "react";
+import Wish from "./Wish";
+import Paper from "@material-ui/core/Paper";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
 
-const Wishlist = () => {
+export default function WishList({ wishes, toggleWish, deleteWish, editWish }) {
   return (
-    <div>
-      <h1>Wishlist</h1>
-    </div>
+    <Paper>
+      <List>
+        {wishes.map((wish, index) => (
+          <div key={wish.id}>
+            <Wish
+              {...wish}
+              toggleWish={toggleWish}
+              deleteWish={deleteWish}
+              editWish={editWish}
+            />
+            {/* only render a Divider if the index of
+            this individual wish is not the last one */}
+            {index < wishes.length - 1 && <Divider />}
+          </div>
+        ))}
+      </List>
+    </Paper>
   );
-};
-
-export default Wishlist;
+}
--- client/src/pages/profile/Profile.js
+++ client/src/pages/profile/Profile.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import Wishlist from '../../components/wishlist/Wishlist.js';
+import Wishlist from '../../components/wishlist/WishListApp.js';
 import './profile.css';
 
 const Profile = () => {
     return (
-        <div
-            style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            flexDirection: 'column',
-            height: '90vh'
-        }}
-    >
+        <div>
       <h1>Profile</h1>
       <Wishlist />
     </div>
