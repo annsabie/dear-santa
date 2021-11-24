f40c036c2fd2975c1beb1c8b61b6ed2908cf399c
Cole Enyart <letchro@gmail.com>
2021-11-24 12:01:25 -0600

    fix

---
+++ client/src/pages/profile/Profile.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Wishlist from '../../components/wishlist/WishListApp.js';
+import './profile.css';
+
+// This is the profile page updated
+const Profile = () => {
+    return (
+        <div>
+      <h1>Profile</h1>
+      <Wishlist />
+    </div>
+    );
+};
+
+export default Profile;
