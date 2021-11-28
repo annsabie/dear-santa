d4ad5dccb8f190ecf92ea3b38cc3ed015d723a95
Frost <mpyang2021@gmail.com>
2021-11-27 23:51:13 -0600

    updated grid on about us to function properly

--- client/src/pages/about/about.js
+++ client/src/pages/about/about.js
@@ -73,6 +73,8 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import Santa from '../../images/santa.jpg'
+import Winter from '../../images/winter.jpg'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -85,44 +87,49 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+
 export default function FullWidthGrid() {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
+      <Grid container justifyContent="center" spacing={3} style={{ margin: "0px 0px 0px 0px"}}>
+        <Grid item xs={12} sm={5}>
+       
           <Paper className={classes.paper}>Dear Santa! Our Story</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>>We can agree there is so much time and money spent during
+        </Grid></Grid>
+
+        <Grid container justifyContent="center" spacing={3} style={{ margin: "0px 0px 300px 0px"}}>
+        <Grid item justifyContent="center" xs={12} sm={5}>
+
+       
+          <Paper className={classes.paper}>
+            
+                    
+                  <img src={Santa} style={{
+                    width: "80%"
+                  }}></img>
+                    
+                    <p>We can agree there is so much time and money spent during
                     holiday season just trying to figure out which gift to buy,
                     or if someone had already bought the gift that you are still
-                    waiting in line for.</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>Imagine waking up for Christmas knowing you will not receive
+                    waiting in line for.
+                    Imagine waking up for Christmas knowing you will not receive
                     the same gift from your favorite aunties or going gift
-                    shopping, knowing exactly what to add to your shopping cart.</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>Here at Dear Santa, we have made it our priority to equip
-                    you with tool to be able to navigate your holiday season
-                    shopping adventures.</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>You can see what items have been purchased and what is
-//                     still available to be changed.</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>Effortless organization of
-//                     your gifts.</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>All you need is a link to your item to add it to
-//                     your wish list and have an ability to rank your items.</Paper>
-        </Grid>
-      </Grid>
+                    shopping, knowing exactly what to add to your shopping cart.
+                    </p>
+                    <p>      
+                    Here at Dear Santa, we have made it our priority to equip
+                    you with tool to be able to navigate your holiday season                    
+                    All you need is a link to your item to add it to
+                    your wish list and have an ability to rank your items.
+                    You can see what items have been purchased and what is
+                    still available to be changed -
+                    Effortless organization of
+                    your gifts</p></Paper>
+        </Grid></Grid>
+
+     
     </div>
   );
 }
