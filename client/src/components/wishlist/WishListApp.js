5eb5d66176ba97d4f99e5b5ba3e13340323b8904
Cassie Meyer <cassie.meyer@leadpages.com>
2021-11-30 17:48:31 -0600

    added styling.

--- client/src/components/wishlist/WishListApp.js
+++ client/src/components/wishlist/WishListApp.js
@@ -1,17 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import WishForm from './WishForm';
-import WishList from './Wishlist';
-import useWishesState from '../../hooks/useWishesState';
-import { WishesProvider } from '../../context/wishes.context';
-import AppBar from '@material-ui/core/AppBar';
-import Paper from '@material-ui/core/Paper';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { v4 as uuid } from 'uuid';
+
+import React, { useEffect, useState } from "react";
+import WishForm from "./WishForm";
+import WishList from "./Wishlist";
+import useWishesState from "../../hooks/useWishesState";
+import { WishesProvider } from "../../context/wishes.context";
+import AppBar from "@material-ui/core/AppBar";
+import Paper from "@material-ui/core/Paper";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { v4 as uuid } from "uuid";
 // import uuid from 'uuid/v4';
-import Grid from "@material-ui/core/Grid"
-
-
+import Grid from "@material-ui/core/Grid";
 export default function WishListApp() {
   const initialWishesArray = [
     // {
@@ -21,49 +20,46 @@ export default function WishListApp() {
     //   link: 'https://www.audio-technica.com/cms/headphones/99aff89488ddd6b1/index.html'
     // }
   ];
-
   // const wishesLength = JSON.parse(localStorage.getItem('wishes')).length;
   const wishesLength = 0;
-
   const initialWishes =
     wishesLength > 0
       ? // if there are any wishes, render them
-        JSON.parse(window.localStorage.getItem('wishes'))
+        JSON.parse(window.localStorage.getItem("wishes"))
       : // otherwise, in case the user deleted all, reload hardcoded wishes from
         // initialWishesArray upon refresh so it won't start as empty
         initialWishesArray;
-
-  const { wishes, addWish, deleteWish, toggleWish, editWish } = useWishesState(initialWishes);
-
-  useEffect(
-    () => {
-      // sync wishes to localStorage by saving them under the key of "wishes"
-      // since wishes are objects, and localStorage needs a string,
-      // we need to explicitly convert them into strings, in order to avoid
-      // the JS Engline implicitly calling .toString() on the objects
-      window.localStorage.setItem('wishes', JSON.stringify(wishes));
-    },
-    [ wishes ]
-  );
-
+  const { wishes, addWish, deleteWish, toggleWish, editWish } =
+    useWishesState(initialWishes);
+  useEffect(() => {
+    // sync wishes to localStorage by saving them under the key of "wishes"
+    // since wishes are objects, and localStorage needs a string,
+    // we need to explicitly convert them into strings, in order to avoid
+    // the JS Engline implicitly calling .toString() on the objects
+    window.localStorage.setItem("wishes", JSON.stringify(wishes));
+  }, [wishes]);
   return (
-    <Paper
-      style={{
-        height: '100vh',
-        backgroundImage: 'linear-gradient(to right top, #d1bebc, #d2c0c1, #c5b0ba, #b2a3b6, #9798b2, #768faa)',
-        padding: 0,
-        margin: 0
-      }}
-      elevation={0}
-    >
-      <Grid container justify='center' style={{ marginTop: '1rem' }}>
-        <Grid item xs={11} md={9} lg={5}>
-          <WishesProvider>
-            <WishForm addWish={addWish} />
-            <WishList wishes={wishes} toggleWish={toggleWish} deleteWish={deleteWish} editWish={editWish} />
-          </WishesProvider>
-        </Grid>
+    // <Paper
+    //   style={{
+    //     height: "100vh",
+    //     padding: 0,
+    //     margin: 0,
+    //   }}
+    //   elevation={0}
+    // >
+    <Grid container justify="center" style={{ marginTop: "1rem" }}>
+      <Grid item xs={11} md={9} lg={5}>
+        <WishesProvider>
+          <WishForm addWish={addWish} />
+          <WishList
+            wishes={wishes}
+            toggleWish={toggleWish}
+            deleteWish={deleteWish}
+            editWish={editWish}
+          />
+        </WishesProvider>
       </Grid>
-    </Paper>
+    </Grid>
+    // </Paper>
   );
 }
